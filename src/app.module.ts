6c18e6e72f8b077c32729be73a731f089d76dd9c
Avner Hattab <avner.h@nanox.vision>
2025-01-22 14:14:54 +0200

    wrap in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { TodosModule } from './todos/todos.module';
-import { ConfigModule } from '@nestjs/config';
 import { LoggerModule } from './logger/logger.module';
 import { AppConfigModule } from './app-config/app-config.module';
-import * as Joi from 'joi';
 
 @Module({
   imports: [DatabaseModule, TodosModule, LoggerModule, AppConfigModule],
